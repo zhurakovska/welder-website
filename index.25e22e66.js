a65888eeeca28056d27086fb661d3c189f2d78cd
zhurakovska <109520904+zhurakovska@users.noreply.github.com>
2023-08-01 16:36:05 +0000

    Deploying to gh-pages from @ zhurakovska/welder-website@f62ac784adcba7c68adada0b99ed9b1cc5305df9 🚀

---
+++ index.25e22e66.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-open-button"),closeMenuBtn:document.querySelector(".menu-close-button"),menu:document.querySelector(".mobile-menu")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.25e22e66.js.map
---
+++ index.8b53a739.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".menu-open-button"),closeMenuBtn:document.querySelector(".menu-close-button"),menu:document.querySelector(".mobile-menu")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.8b53a739.js.map
