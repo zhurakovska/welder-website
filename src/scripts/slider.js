fe61703e5c50f574534bf8c578b53367793506a2
zhurakovska <n.sinawka@gmail.com>
2023-08-02 00:09:22 +0300

    add styles slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,33 +1,40 @@
-import $ from "jquery";
+import $ from 'jquery';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-$(document).ready(function() {
-    $('.slider').slick({
-        autoplaySpeed: 3000,
-        infinite: true,
-        slidesToShow: 3,
-        arrows: false, 
-        prevArrow: $(".custom-prev-arrow"),
-        nextArrow: $(".custom-next-arrow"),
-        
-    
-        centerMode: true, // Включить режим центрирования
-        centerPadding: 0,
+$(document).ready(function () {
+  $('.slider').slick({
+    autoplaySpeed: 3000,
+    infinite: true,
+    slidesToShow: 3,
+    arrows: false,
+    prevArrow: $('.custom-prev-arrow'),
+    nextArrow: $('.custom-next-arrow'),
 
-    });
+    centerMode: true, // Включить режим центрирования
+    centerPadding: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
 
-    $(".custom-prev-arrow").click(function() {
-        $(".slider").slick("slickPrev");
-    });
+  $('.custom-prev-arrow').click(function () {
+    $('.slider').slick('slickPrev');
+  });
 
-    $(".custom-next-arrow").click(function() {
-        $(".slider").slick("slickNext");
-    });
+  $('.custom-next-arrow').click(function () {
+    $('.slider').slick('slickNext');
+  });
 
-    $(window).on("resize", function() {
-        // Вручную обновляем слайдер при изменении размера окна
-        $(".slider").slick("setPosition");
-      });
+  $(window).on('resize', function () {
+    // Вручную обновляем слайдер при изменении размера окна
+    $('.slider').slick('setPosition');
+  });
 });
